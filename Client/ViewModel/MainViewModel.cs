5fac1e0c1795453b8fa90a33968f95046748d772
Elxan Atayev <elxanatayev.la@gmail.com>
2021-12-30 00:23:52 +0400

    Add project files.

---
+++ Client/ViewModel/MainViewModel.cs
@@ -0,0 +1,96 @@
+﻿using Client.Command;
+using Client.Special;
+using Client.View;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Client.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public RelayCommand SelectBtnCommand { get; set; }
+        public RelayCommand SendBtnCommand { get; set; }
+
+
+        byte[] b;
+
+
+        public MainViewModel(MainWindow mainWindow)
+        {
+
+
+
+            SelectBtnCommand = new RelayCommand((sender) =>
+            {
+
+                try
+                {
+                    var open = new Microsoft.Win32.OpenFileDialog();
+
+                    open.Multiselect = false;
+                    open.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    open.ShowDialog();
+                    b = Helper.GetBytesOfImage(path: open.FileName);
+                    mainWindow.image1.Source = new BitmapImage(new Uri(open.FileName));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+
+
+
+
+
+
+
+            });
+            SendBtnCommand = new RelayCommand((sender) =>
+            {
+                var client = new TcpClient();
+                var ip = IPAddress.Parse("192.168.1.103");
+                var port = 27001;
+                var ep = new IPEndPoint(ip, port);
+                try
+                {
+                    client.Connect(ep);
+                    if (client.Connected)
+                    {
+                        var writer = Task.Run(() =>
+                        {
+                            while (true)
+                            {
+
+                                var stream = client.GetStream();
+                                var bw = new BinaryWriter(stream);
+                                bw.Write(b);
+                            };
+                        });
+
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("CLinet doesnt connected");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            });
+
+        }
+    }
+}
---
+++ ImageSenderProgram/ViewModel/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Client.Model;
+using Client.Special;
+using ImageSenderProgram.Command;
+using ImageSenderProgram.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ImageSenderProgram.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public RelayCommand a { get; set; }
+        public List<Images> images { get; set; }
+        public string Path { get; set; }
+        static TcpListener listener = null;
+        static BinaryWriter bw = null;
+        static BinaryReader br = null;
+        int count = 0;
+        public MainWindowViewModel(MainWindow mainWindow)
+        {
+
+            images = new List<Images>();
+
+            a = new RelayCommand((sender) =>
+            {
+                Task.Run(() =>
+                {
+                    var ip = IPAddress.Parse("192.168.1.103");
+                    var ep = new IPEndPoint(ip, 27001);
+                    listener = new TcpListener(ep);
+                    listener.Start();
+                    while (true)
+                    {
+                        var client = listener.AcceptTcpClient();
+                        MessageBox.Show($"{client.Client.RemoteEndPoint} connected");
+                        byte[] bytes = new byte[50000000];
+                        Task.Run(() =>
+                        {
+                            var reader = Task.Run(() =>
+                            {
+                                ++count;
+                                var stream = client.GetStream();
+                                br = new BinaryReader(stream);
+                                bw = new BinaryWriter(stream);
+                                while (true)
+                                {
+                                    bytes = br.ReadBytes(count: 5000000);
+                                    var msg = br.ReadString();
+                                    Path = Helper.GetImagePath(buffer: bytes, counter: count);
+                                    break;
+                                }
+                                App.Current.Dispatcher.Invoke(() =>
+                                {
+                                    mainWindow.myListView.Items.Add(new Images { ImagePath = Path });
+                                });
+                            });
+                        });
+                    }
+                });
+            });
+        }
+    }
+}
